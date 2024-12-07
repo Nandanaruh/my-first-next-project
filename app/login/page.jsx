2dd9b95eb02690112d5ea9891b5f47963920f3d0
nandanaruh <nandanaruh@gmail.com>
2024-12-08 00:13:41 +0530

    Create LoginForm component

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,9 @@
-"use client";
-
+import LoginForm from "./login-form";
+//Server component for SSR - Sever Side Rendering
 export default function LoginPage() {
   return (
-    <div>
-      
+    <div className="flex flex-col justify-center items-center min-h-screen text-black">
+      <LoginForm title ="Sign in to LMS"/>
     </div>
-  )
+  );
 }
