a7af75320d109aa4a95bf1c9f308bc93e4d52f92
nandanaruh <nandanaruh@gmail.com>
2024-12-07 19:49:13 +0530

    create login,about and

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function AboutUs() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-yellow-200 text-black text-center">
+      <div className=" bg-green-300 px-5 py-3 text-black rounded-lg">
+        AboutUs
+      </div>      
+    </div>
+  )
+}
