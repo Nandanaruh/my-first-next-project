906ac180fd017207bf7e73e7042a95f1762f3716
nandanaruh <nandanaruh@gmail.com>
2024-12-27 16:32:58 +0530

    Create mongoDb.js and add database link to .env file

--- app/api/v1/login/route.js
+++ app/api/v1/login/route.js
@@ -3,5 +3,12 @@ import { NextResponse } from "next/server";
 export const POST = async (req) => {
     const request = await req.json();
     console.log(request);
+
+    //Bind database.
+    //Find the user in database.
+    //Check password validity.
+    //Return the response with  token.
+    //If password invalid return error response
+    
     return NextResponse.json({ success: true, username: "Nandana" });
 }
