7ddd50eaa7dbfc7df2600f7abb93b3e42984a96a
nandanaruh <nandanaruh@gmail.com>
2024-12-27 11:20:21 +0530

    Code for request body.

--- app/api/v1/login/route.js
+++ app/api/v1/login/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
-    console.log(req);
+    const request = await req.json();
+    console.log(request);
     return NextResponse.json({ success: true, username: "Nandana" });
 }
