9db125a039a27337d3cb5eb6f3f8abf871145b30
nandanaruh <nandanaruh@gmail.com>
2024-12-28 00:01:18 +0530

    Create shadcn card, code for get movies in server.js and code api.js

--- app/lib/apis/server.js
+++ app/lib/apis/server.js
@@ -1,3 +1,6 @@
+import { HTTPError } from "ky";
+import { api } from "../apis/api"
+
 export const loginUser = async (loginData) => {
     //console.log(loginData);
     const response = await fetch("http://localhost:3000/api/v1/login", {
@@ -9,3 +12,26 @@ export const loginUser = async (loginData) => {
     });
     console.log("LOGIN ACTION", response.json());
 } 
+
+export const getMovies = async () => {
+    try {
+        const response = await api.get("movies");
+        return response;
+    } catch (error) {
+        if (error) {
+            const status = error.response.status;
+            const responseBody = await error.response.json();
+            console.log("HTTPerror", status, responseBody);
+            // if (status === 401) {
+            //     console.log("Unauthorized, check your credentials.");
+            // } else if (status === 500) {
+            //     console.log("Server error, please try later.");
+            // } else if (status === 404) {
+            //     console.log(status, responseBody.message);
+            // }
+        } else {
+            console.log("Unknown error", error);
+        }
+        return undefined;
+    }
+};
