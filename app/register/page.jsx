24da7d8a2d4202dd304da8bedcaa28d213a40782
nandanaruh <nandanaruh@gmail.com>
2025-01-02 20:51:06 +0530

    Edit regiter page and update it using shadcn Card, Label, Input and Button elemrnts.

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -2,8 +2,11 @@ import RegisterForm from "./register-form";
 //Server component for SSR - Sever Side Rendering
 export default function RegisterPage() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen text-black">
-      <RegisterForm title="Register"/>
+    // <div className="flex flex-col justify-center items-center min-h-screen text-black">
+    //   <RegisterForm title="Register"/>
+    // </div>
+    <div className="container mx-auto">
+      <RegisterForm title="Register" />
     </div>
   );
 }
