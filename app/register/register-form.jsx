30d84dcd505a04a213c6471cb862e0a8ea1f7f1b
nandanaruh <nandanaruh@gmail.com>
2025-01-02 22:40:47 +0530

    Create register page and link login page and register page.

--- app/register/register-form.jsx
+++ app/register/register-form.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Card,
   CardContent,
@@ -9,35 +10,74 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@radix-ui/react-label";
-import { Input } from "postcss";
 //Keep this as a client component
 import { useState } from "react";
+import Link from "next/link";
 
 export default function RegisterForm({ title }) {
-  const [email, setEmail] = useState("nandana@gmail.com");
-  const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({ email: email, password: password });
+  const handleSubmitForm = async (event) => {
+    event?.preventDefault();
+    const formData = new FormData(event?.currentTarget);
+    const name = formData.get("name") ?? "";
+    const email = formData.get("email") ?? "";
+    const password = formData.get("password") ?? "";
+    const confirmPassword = formData.get("confirmPassword") ?? "";
+    console.log("Submit", { name, email, password, confirmPassword });
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Card className="bg-blue-50/90 w-[380px]">
         <CardHeader>
-          <CardTitle>Create an account</CardTitle>
-          <CardDescription>Enter your information to get start</CardDescription>
+          <CardTitle className="text-center text-xl font-semibold text-gray-900">
+            Create an account
+          </CardTitle>
+          <CardDescription className="text-center text-sm bg-gray-500/50 text-white rounded inline-block">
+            Enter your information to get start
+          </CardDescription>
         </CardHeader>
-        <form>
+        <form onSubmit={handleSubmitForm}>
           <CardContent>
-            <div>
-              <div>
-                <Label>Email</Label>
-                <Input></Input>
+            <div className="flex flex-col space-y-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" name="name" placeholder="Jone Doe" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  placeholder="yourname@example.com"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Enter new password"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="confirmPassword">Confirm Password</Label>
+                <Input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Enter password again to confirm"
+                />
+              </div>
+              <div className="flex justify-center gap text-xs">
+                Already have an account ?
+                <Link href="/login" className="text-blue-600 hover:underline">
+                  Login
+                </Link>
               </div>
             </div>
           </CardContent>
-          <CardFooter className="flex justify-center ">
+          <CardFooter className="flex justify-center">
             <Button className="flex-1" type="submit">
               Register
             </Button>
