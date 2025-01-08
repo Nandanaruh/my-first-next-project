e6790b5c87fe2b7b4a5294b62678318e541e5923
nandanaruh <nandanaruh@gmail.com>
2025-01-08 21:34:53 +0530

    Code to check existing user in users.

--- app/api/v1/register/route.js
+++ app/api/v1/register/route.js
@@ -15,7 +15,7 @@ export const POST = async (req) => {
         { status: 400 },
       );
     }
-
+    //TODO:: Do further validation
     const client = await clientPromise();
     const db = client.db("sample_mflix");
     const existingUser = await db.collection("users").findOne({ email });
@@ -27,14 +27,27 @@ export const POST = async (req) => {
       );
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log("Hashed Password", hashedPassword);
     const result = await db.collection("users").insertOne({
       name,
       email,
       password: hashedPassword,
       createdAt: new Date(),
     });
-    return NextResponse.json({ success: true, username: name, email: email });
+    if (result && result.acknowledged) {
+      return NextResponse.json({
+        success: true,
+        user: {
+          _Id: result.insertedId,
+          username: name,
+          email: email,
+        },
+      });
+    } else {
+      return NextResponse.json(
+        { error: "Internal server error" },
+        { status: 500 },
+      );
+    }
   } catch (error) {
     console.log("Mongodb error", error);
     return NextResponse.json(
