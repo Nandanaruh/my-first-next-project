9cdc6177a91a1fe799ab2b44a1cd4e032144646c
nandanaruh <nandanaruh@gmail.com>
2025-01-13 23:58:23 +0530

    Create user navigation bar in dashboard, add dropdown menu and avatar in shadcn ui. So manage session to register and logout.

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,23 @@
+import UserNav from "./components/user-nav";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="flex h-screen overflow-hidden bg-gray-100">
+      {/* Side panel */}
+      <aside className="w-64 overflow-y-auto border-r bg-white shadow-lg">
+        Side panel
+      </aside>
+      <div className="bg-pink-100 flex flex-1 flex-col overflow-hidden">
+        {/* Dashboard header */}
+        <header className="flex bg-white h-16 items-center justify-between gap-4 border-b px-6 shadow-sm">
+          <h1 className="text-2xl font-bold text-blue-700">MFlix Dashboard</h1>
+          <UserNav className="end" />
+        </header>
+        {/* Dashboard page */}
+        <main className="flex flex-1 overflow-y-auto bg-gray-100 p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
