8445c37b1977b312d2750ae57d5a65a1f60af721
nandanaruh <nandanaruh@gmail.com>
2025-01-24 19:07:48 +0530

    Edit about page.

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,10 +2,10 @@
 
 export default function AboutUs() {
   return (
-    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-yellow-200 text-black text-center">
-      <div className=" bg-green-300 px-5 py-3 text-black rounded-lg">
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-red-200 text-black text-center">
+      <div className=" bg-green-300 px-5 py-3 text-white rounded-lg">
         AboutUs
-      </div>      
+      </div>
     </div>
-  )
+  );
 }
