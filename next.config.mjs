8fdd6677f241c49a4edc1d170f8289113b3ca235
nandanaruh <nandanaruh@gmail.com>
2025-01-24 18:34:05 +0530

    Try to resolve image link error on poster field.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,32 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     // domains: ["m.media-amazon.com"] },
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "m.media-amazon.com",
+//         port: "",
+//         search: "",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["m.media-amazon.com"] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
