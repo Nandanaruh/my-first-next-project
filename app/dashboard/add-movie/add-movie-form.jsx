da3bb44d1994c9841ce53a32ba5857c4198e71fb
nandanaruh <nandanaruh@gmail.com>
2025-01-31 00:36:41 +0530

    Add movies page is finished and displayed in dashboard.

--- app/dashboard/add-movie/add-movie-form.jsx
+++ app/dashboard/add-movie/add-movie-form.jsx
@@ -11,10 +11,42 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+  SelectItem,
+} from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { MultiSelect } from "@/components/multi-select";
+import { GENRES, RATINGS } from "@/app/lib/constants";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+import { createMovie } from "@/app/lib/actions/movies";
 
 export default function AddMovieForm() {
+  const [genres, setGenres] = useState([]);
+  const [rated, setRated] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const genresList = GENRES.map((genre) => ({
+    label: genre,
+    value: genre,
+  }));
+
+  const handleSubmitForm = async (event) => {
+    event?.preventDefault();
+    const formData = new FormData(event?.currentTarget);
+    const title = formData.get("title").toString();
+    const plot = formData.get("plot").toString();
+    const year = Number(formData.get("year"));
+
+    if (title && year && plot) {
+      setLoading(true);
+      await createMovie({ title, year, plot, genres, rated });
+      setLoading(false);
+    }
+  };
   return (
     <Card className="max-w-2xl mx-auto">
       <CardHeader>
@@ -25,14 +57,14 @@ export default function AddMovieForm() {
           Add a movie to Mflix Dashboard.
         </CardDescription>
       </CardHeader>
-      <form>
+      <form onSubmit={handleSubmitForm}>
         <CardContent className="space-y-4">
           <div>
             <Label htmlFor="title">Movie Title</Label>
             <Input id="title" name="title" placeholder="Enter movie title" />
           </div>
           <div>
-            <Label htmlFor="title">Movie Year</Label>
+            <Label htmlFor="year">Movie Year</Label>
             <Input
               id="year"
               name="year"
@@ -50,10 +82,37 @@ export default function AddMovieForm() {
           </div>
           <div>
             <Label htmlFor="genres">Movie Genres</Label>
-            <MultiSelect list={[]} placeholder="Select Movie Genres" />
+            <MultiSelect
+              list={genresList}
+              placeholder="Select Movie Genres"
+              onValueChange={setGenres}
+            />
+          </div>
+          <div>
+            <Label htmlFor="rated">Movie Rated</Label>
+            <Select onValueChange={(val) => setRated(val)}>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a rating" />
+              </SelectTrigger>
+              <SelectContent>
+                {RATINGS.map((rating) => (
+                  <SelectItem key={rating} value={rating}>
+                    {rating}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
           </div>
         </CardContent>
-        <CardFooter className="w-full flex justify-end space-x-2"></CardFooter>
+        <CardFooter className="w-full flex justify-end space-x-2">
+          <Button type="reset" variant="outline">
+            Clear Form
+          </Button>
+          <Button type="submit" disabled={isLoading}>
+            {isLoading && <Loader2 className="animate-spin" />}
+            Add Movie
+          </Button>
+        </CardFooter>
       </form>
     </Card>
   );
--- app/lib/actions/movies.js
+++ app/lib/actions/movies.js
@@ -1,10 +1,12 @@
 //Movie related server actions
+
+"use server";
 import clientPromise from "@/app/lib/mongodb";
 export const createMovie = async (movie) => {
   try {
     const client = await clientPromise();
     const db = client.db("sample_mflix");
-    const result = await db.collection("movies-new").insertOne(movie);
+    const result = await db.collection("movies_new").insertOne(movie);
 
     console.log(`A movie was added with _id:${result.insertedId}`);
   } catch (error) {
