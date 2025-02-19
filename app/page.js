5a54857996114a9410577e6ee2683b0f8fc3ba43
nandanaruh <nandanaruh@gmail.com>
2025-02-19 20:43:46 +0530

    Modified design of the localhost page.jsx

--- app/page.js
+++ app/page.js
@@ -1,51 +1,31 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col items-center justify-center">
-        <Image
-          className="rounded-full mb-5 border border-gray-400 brightness-100 transition-transform duration-300 hover:brightness-110 hover:scale-105 hover:shadow-lg"
-          src="/nandana.jpg"
-          alt="Nandana Rathanayaka"
-          title="Nandana Rathanayaka"
-          width={300}
-          height={300}
-          priority
-        />
-        <ol className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            <h4 className="text-center text-xl font-semibold text-gray-900">
-              Welcome to Evotec Mflix Movie Dashboard.{" "}
-            </h4>
-          </li>
-          <li>
-            <div className="flex flex-row space-x-4 items-center justify-start">
-              <div className="text-sm text-gray-500 font-medium space-x-1">
-                <Button variant="outline">
-                  <a
-                    href="/register"
-                    className="text-xs text-gray-700 no-underline"
-                  >
-                    Sign Up
-                  </a>
-                </Button>
-              </div>
-              <div className="text-xs text-gray-500 font-medium  space-x-1">
-                <Button variant="outline">
-                  <a
-                    href="/login"
-                    className="text-xs text-gray-700 no-underline"
-                  >
-                    Sign In
-                  </a>
-                </Button>
-              </div>
-            </div>
-          </li>
-        </ol>
-      </main>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="m-5">
+        <h1 className="font-sans font-bold text-center text-6xl md:text-5xl lg:text-6xl leading-snug text-gray-700">
+          Welcome to Evotec <br />
+          Mflix Movie Dashboard
+        </h1>
+      </div>
+      <div className="flex flex-row space-x-4">
+        <div>
+          <Button className="bg-blue-500 w-[120px] hover:bg-blue-400">
+            <a href="/login" className="text-xs text-blue-950 no-underline">
+              Login
+            </a>
+          </Button>
+        </div>
+        <span>or</span>
+        <div>
+          <Button className="bg-green-500 w-[120px] hover:bg-green-400">
+            <a href="/register" className="text-xs text-green-950 no-underline">
+              Register
+            </a>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
