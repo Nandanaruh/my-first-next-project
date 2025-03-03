4b77661a2352c954970a9f2aa5e476bfb271b535
nandanaruh <nandanaruh@gmail.com>
2025-03-03 08:34:55 +0530

    Add contruction image to settings page.

--- app/dashboard/settings/page.jsx
+++ app/dashboard/settings/page.jsx
@@ -1,13 +1,18 @@
-import { Wrench, FileCode2, Construction } from "lucide-react";
+import Image from "next/image";
 export default function SettingsPage() {
   return (
     <div className="flex flex-col justify-center items-center mt-60">
       <div className="flex flex-row items-center justify-center">
-        <div className="-my-5">
-          <Wrench className="text-green-500 size-10" />
-        </div>
-        <div>
-          <FileCode2 className="text-red-500 size-20 -mx-2" />
+        <div className="-my-0">
+          <Image
+            src="/construction-tool.png"
+            alt="construction-tool"
+            title="Construction Tool"
+            width={160}
+            height={160}
+            className="w-20 h-auto aspect-auto brightness-90 transition-transform duration-300 hover:brightness-105 hover:scale-105 hover:shadow-lg"
+            priority
+          />
         </div>
       </div>
       <div>
