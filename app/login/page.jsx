77b31ef6b0798134134303447e04e2256001be46
nandanaruh <nandanaruh@gmail.com>
2025-03-09 23:48:15 +0530

    Fix size of login page 2

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,7 +2,6 @@ import LoginForm from "./login-form";
 //Server component for SSR - Sever Side Rendering
 export default function LoginPage() {
   return (
-    // <div className="flex flex-col justify-center items-center bg-blue-50/90  min-h-screen text-black px-4">
     <div className="bg-blue-50/90 container mx-auto">
       <LoginForm title="Sign in to Dashboard" />
     </div>
